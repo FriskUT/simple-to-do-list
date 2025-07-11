97f2c633eea1998395e145b848987945b8b00e07
Chara Dreemurr <charadreemurr278@gmail.com>
2025-07-11 16:19:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,10 +14,9 @@ options = '''
 [1] Add something to the list
 [2] Remove something from the list
 [3] See the current list
-[Any Other Number] Exit
+[4] Exit
 '''
-my_list = []
-
+# This prints the beginning text with ASCII art, a question and options for the user.
 def beggining_function():
     print(ascii_art)
     print(question)
@@ -25,35 +24,53 @@ def beggining_function():
 
 beggining_function()
 
+# This function uses a txt file to store a list of items, making the list last pernamently between runs.
 def add_to_list():
     add_item = input("What do you want to add? Add only one thing at a time.:")
-    my_list.append(add_item)
-
+    with open("list.txt", "a") as file:
+        file.write(add_item + "\n")
+    
+# This function removes a item from the list, by reading the contents and rewriting the file without the specified item.
 def remove_from_list():
+    # This asks the user for the item they want to remove. If they don´t type the exact text, the script will automatically crash.
+    # It is planned to fix the bug that makes the script crash if the user types something that is not in the list.
     remove_item = input("What do you want to remove? Please type the exact text of the item you want to remove:")
-    my_list.remove(remove_item)
+    with open("list.txt", "r") as file:
+        lines = file.readlines()
+    with open("list.txt", "w") as file:
+        for line in lines:
+            if remove_item not in line:
+                file.write(line)
 
+def print_options():
+    print(options) # This function prints the options for the user, in case they just completed another function.
+            
+# This function reads the contents of the list, makes a variable with the contents and prints it.    
 def see_list():
-    if not my_list:
-        print("The list is empty.")
-    else:
-        print("Current list:")
-        for item in my_list:
-            print(item)
+    with open("list.txt", "r") as file:
+        contents = file.read() # reads the contents of the file
+    print(contents)  # prints everything in the file
+    
 
 while True:
     try:
         option = int(input("Please choose a option:"))
     except ValueError:
-        print("Please enter a valid number.")
+        print("Please enter a valid number.") # This will happen if the user types something that is not a number.
         continue
 
     if option == 1:
         add_to_list()
+        print_options()
     elif option == 2:
         remove_from_list()
+        print_options()
     elif option == 3:
         see_list()
-    else:
-        print("Exiting the script. Goodbye!")
+        print_options()
+    elif option == 4:
+        print("Exiting the program. Goodbye!")
         break
+    else:
+        print("Invalid option. Please choose a valid option from the menu")
+        print_options()
